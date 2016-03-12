2db8b20cbbce01b68b90b490fc0785adac607e14
masterbase <masterbase@masterbase.at>
2016-03-13 00:56:39 +0100

    fix for importer.py is broken, revert

--- importer.py
+++ importer.py
@@ -52,9 +52,8 @@ def get_schedule(url):
                 title = text_or_empty(event, 'title'),
                 place = text_or_empty(event, 'room'),
                 speakers = [
+                    unicode(person.text.strip()) 
                     for person in persons
-                        if person is not None:
-                            unicode(person.text.strip()) 
                 ] if persons else [],
                 lang = text_or_empty(event, 'language') or "unk",
                 id = event.attrib["id"],
